4579755d116f8b14449f9a148d85e34e6bbbe41e
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-29 15:21:34 +0530

    s

--- public/WebWorker/topProductsWorker.ts
+++
@@ -1,9 +0,0 @@
-// topProductsWorker.ts
-import { getTopProducts } from '@/services/spot-prices';
-const ctx: Worker = self as any;
-
-ctx.addEventListener('message', async (event) => {
-  const { metalType } = event.data;
-  const response = await getTopProducts('NearToSpot', '', metalType,'6','1');
-  ctx.postMessage(response.homePageProductDetails);
-});
